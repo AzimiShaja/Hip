b492476111e907ea09c4dca8af96f8a607584e24
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-07-07 12:43:50 +1000

    finished merchandising component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import Cards from "@/components/Cards";
 import Hero from "@/components/Hero";
+import Merchandising from "@/components/Merchandising";
 import Perks from "@/components/Perks";
 import Testimonials from "@/components/Testimonials";
-import React, { useState } from "react";
 
 const Home = () => {
 	return (
-		<div className="w-full py-5 flex items-center justify-center flex-col gap-5">
+		<div className="w-full py-5 flex items-center justify-center flex-col gap-10">
 			<Hero />
 			<Cards />
 			<Perks />
 			<Testimonials />
+			<Merchandising />
 		</div>
 	);
 };
---
+++ components/Merchandising.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Merchandising = () => {
+    return (
+        <div className="xl:w-[1400px] w-full px-8 flex items-center flex-col bg-orange-button mt-20 p-20 rounded-3xl gap-20">
+            <h1 className="xl:text-[120px] text-3xl md:text-6xl lg:text-7xl xl:max-w-[900px] font-extrabold text-center xl:leading-[120px]">
+                Merchandising made simple.
+            </h1>
+            <div className="flex items-center gap-5 max-xl:flex-col max-xl:w-full">
+                <button className="bg-gray-grid-1 text-white rounded-xl p-5 px-10 text-md hover:opacity-90 transition-all max-lg:w-full">
+                    Try for Free
+                </button>
+                <button className="border border-black rounded-xl p-5 px-8 text-md hover:opacity-90 transition-all max-lg:w-full">
+                    Book a demo
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Merchandising;
