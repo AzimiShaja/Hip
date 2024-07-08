3a4eb13e8ab6f14dc634f09c5f3a0dd7ca207ccd
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-07-08 15:04:13 +1000

    fixed anchor links

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,10 +4,11 @@ import { CustomLink } from "./Header";
 import { motion } from "framer-motion";
 import { ToggleNavStatus } from "./context/ToggleNavContext";
 const MobileNav = () => {
-    const { openNav } = useContext(ToggleNavStatus);
+    const { openNav, setOpenNav } = useContext(ToggleNavStatus);
     return (
         <motion.nav
             initial={{ height: 0 }}
+            onClick={() => setOpenNav(false)}
             animate={{ height: openNav ? 330 : 0 }}
             className={`overflow-hidden md:hidden shadow-2xl flex flex-col items-center gap-5 font-bold w-full bg-white h-screen rounded-3xl ${!openNav ? "p-0" : "p-4"
                 } `}
@@ -15,7 +16,7 @@ const MobileNav = () => {
             <CustomLink href="/" placeholder="Home" />
             <CustomLink href="/products" placeholder="Products" />
             <CustomLink href="/pricing" placeholder="Pricing" />
-            <CustomLink href="/signin" placeholder="Sign In" />
+            <CustomLink href="/login" placeholder="Sign In" />
             <button className="bg-orange-button rounded-3xl p-4 font-bold text-md text-white hover:opacity-90 transition-all w-full">
                 Start for Free
             </button>
