744cca63ff9127be902f998b7aa5b089e7dfdeec
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-07-12 20:51:36 +1000

    made inputs required and added signup options

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,6 +3,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import React from "react";
+import { FaFacebook, FaGoogle, FaXing } from "react-icons/fa";
 
 const Signup = () => {
     return (
@@ -13,10 +14,10 @@ const Signup = () => {
                         <h1 className="font-extrabold text-3xl md:text-5xl">Account Creation</h1>
                         <h1 className="font-bold text-gray-400">Create your first Hip account</h1>
                     </div>
-                    <Input className="rounded-xl py-6" placeholder="Full Name" />
-                    <Input className="rounded-xl py-6" type="email" placeholder="Email" />
-                    <Input className="rounded-xl py-6" type="password" placeholder="Password" />
-                    <Input className="rounded-xl py-6" type="password" placeholder="Confirm Password" />
+                    <Input required className="rounded-xl py-6" placeholder="Full Name" />
+                    <Input required className="rounded-xl py-6" type="email" placeholder="Email" />
+                    <Input required className="rounded-xl py-6" type="password" placeholder="Password" />
+                    <Input required className="rounded-xl py-6" type="password" placeholder="Confirm Password" />
                     <div className="flex items-center space-x-2 my-4 w-full">
                         <Checkbox id="terms" className="rounded-[4px] bg-white" />
                         <label
@@ -30,11 +31,14 @@ const Signup = () => {
                         Create Account
                     </button>
                     <p className="text-center">
-                        already have an account?{" "}
-                        <Link className="text-orange-button" href="/login">
-                            Login
-                        </Link>{" "}
+                        By signing up you agree to our{" "}
+                        <span className="text-orange-button cursor-pointer">Terms of Service and Privacy Policy.</span>{" "}
                     </p>
+                    <div className="flex items-center space-x-3 mt-10">
+                        <h1>Sign up with</h1>
+                        <FaGoogle size={30} className="cursor-pointer hover:scale-110 transition-all" />
+                        <FaFacebook size={30} className="cursor-pointer hover:scale-110 transition-all" />
+                    </div>
                 </form>
             </div>
             <Footer />
